cc756e834afef9cef10015830e338ce261cb1ecd
Aditya Tirta <aditya.tirtatjahja@askaradesa.com>
2022-07-07 23:45:44 +0700

    chore: remove sql files

--- askara-palm-patrol/9-activity-type.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS activity_types {
-    id int not null auto_increment,
-    name varchar(255) not null,
-    PRIMARY KEY (id),
-    created_at timestamp default current_timestamp
-}
