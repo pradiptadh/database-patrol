9cf09325722e0d4a036211dd3ddbe66121386a00
Aditya Tirta <aditya.tirtatjahja@askaradesa.com>
2022-07-09 02:14:40 +0700

    feat: connect using params

--- askara-palm-patrol/main.go
+++ askara-palm-patrol/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/Askara-Daulat-Desa/database-patrol/askara-palm-patrol/models"
+	"os"
 )
 
 func main() {
-	models.Connect()
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	models.Connect(&username, &password, &host, &port)
 	models.DB.AutoMigrate(
 		&models.Province{},
 		&models.Regency{},
--- askara-palm-patrol/models/database.go
+++ askara-palm-patrol/models/database.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"os"
+	// "os"
 )
 
-var DSN = fmt.Sprintf("%s:%s@tcp(%s:%s)/askara_palm_patrol", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
 var DB *gorm.DB
 
-func Connect() {
-	db, err := gorm.Open(mysql.Open(DSN), &gorm.Config{})
+func Connect(username *string, password *string, host *string, port *string) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/askara_palm_patrol", *username, *password, *host, *port)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
