9de823e8c59b0f4ef8779bc2c332070228d38806
pradiptadh <haryadipradipta@gmail.com>
2022-08-27 20:08:33 +0700

    Change column position

--- askara-palm-patrol/migrations/mysql/20220826140218-alter_table_labor_block_act.sql
+++ askara-palm-patrol/migrations/mysql/20220826140218-alter_table_labor_block_act.sql
@@ -4,13 +4,13 @@ rename table `askara_palm_patrol`.`labor_block_activites` to labor_block_activit
 rename table `askara_palm_patrol`.`warden_block_activites` to warden_block_activities;
 
 alter table labor_block_activities
-    add note text null;
+    add note text null after work_hour;
 
 alter table labor_block_activities
-    add total_harvest int null;
+    add total_harvest int null after note;
 
 alter table labor_block_activities
-    add has_finished TINYINT not null;
+    add has_finished TINYINT not null after total_harvest;
 
 -- +migrate Down
 alter table labor_block_activities
